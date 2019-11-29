996e64275ec6b81cafd206074536b8ff9bd57de4
DarkCybernetics <Dion.jacksoncs@yahoo.com>
2019-11-29 12:16:51 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+using namespace std;
+/*
+Relational Operators in C++
+
+-----------------------------------------------------------
+== Equal  to
+!= Not equal to
+> Greater Than
+< Less Than
+>= Greater Than or Equal to
+<= Less Than or Equal to
+===============================================================
+DarkCybernetics
+Learn Play Discover Computer Science
+All Rights Reserved 2019.
+www.DarkCybernetics.com
+*/
+int main()
+{
+int a=10;
+int b=20;
+cout<<"Value A:"<<a<<"Value B:"<<b<<endl;
+if (a==b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+if (a!=b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+if (a>b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+if (a<b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+if (a>b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+if (a>=b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+if (a<=b){
+ cout<<"true"<<endl;
+}
+else{
+ cout<<"false"<<endl;
+}
+
+return 0;
+}
